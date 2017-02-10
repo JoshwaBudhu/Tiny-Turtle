1fce0d02b4ed45dbefec64557fa3855dd6181c6e
Joshwa Budhu <joshwabudhu@gmail.com>
2017-02-10 14:09:53 +0000

    changed

---
+++ script.js
@@ -0,0 +1,20 @@
+// JavaScript File
+ TinyTurtle.apply(window); 
+  
+  forward(40);
+  right(90);
+  forward(40);
+  right(90);
+  forward(40);
+  right(90);
+  forward(40);
+  right(90);
+  
+  forward(40);
+  right(35);
+  forward(30);
+  right(102);
+  forward(30);
+  right(90);
+  
+  stamp();
